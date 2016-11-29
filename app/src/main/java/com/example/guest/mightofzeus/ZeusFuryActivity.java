91f4cf2cb11c67b56f7e20259e4dbd945eae4807
Jeremy Fryd and Sandro Alvarez <jeremy+sandro@>
2016-11-29 15:51:32 -0800

    add zeusFuryActivity

---
+++ app/src/main/java/com/example/guest/mightofzeus/ZeusFuryActivity.java
@@ -0,0 +1,13 @@
+package com.example.guest.mightofzeus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ZeusFuryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zeus_fury);
+    }
+}
