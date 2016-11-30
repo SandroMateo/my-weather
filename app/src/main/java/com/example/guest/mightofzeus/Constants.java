274b83639e7d257b0939656f8d51e82318bea512
Jeremy Fryd and Sandro Alvarez <jeremy+sandro@>
2016-11-30 09:59:10 -0800

    add API call

---
+++ app/src/main/java/com/example/guest/mightofzeus/Constants.java
@@ -0,0 +1,12 @@
+package com.example.guest.mightofzeus;
+
+/**
+ * Created by Guest on 11/30/16.
+ */
+public class Constants {
+    public static final String API_KEY = BuildConfig.API_KEY;
+    public static final String API_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/";
+    public static final String CITY_QUERY_PARAMETER = "city";
+    public static final String API_KEY_QUERY_PARAMETER = "appid";
+
+}
